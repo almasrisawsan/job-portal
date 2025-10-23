98acb2a2cd8897b05f36b0a2efc433ae068d406e
almasrisawsan <sawsan.msri@gmail.com>
2025-10-23 08:31:28 +0000

    Create landing page

--- app/root.tsx
+++ app/root.tsx
@@ -35,6 +35,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <body>
         {children}
         <ScrollRestoration />
+        <div>Hello landing page</div>
         <Scripts />
       </body>
     </html>
