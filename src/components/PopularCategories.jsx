edf6033571bdd3a3755bee189d543b06bcac67e7
malik <ma037646@gmail.com>
2025-10-23 12:28:57 +0300

    initial commit

---
+++ src/components/PopularCategories.jsx
@@ -0,0 +1,12 @@
+function PopularCategories() {
+  return (
+    <div>
+      <h2>Popular Categories</h2>
+    </div>
+  );
+}
+function CategoryList() {
+  return <div></div>;
+}
+
+export default PopularCategories;
