b2993f6613af45292b40541ace249e128c016912
Reham Magharee <129688216+RehamRaed@users.noreply.github.com>
2025-10-23 13:49:52 +0300

    create job details page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,23 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Link } from "react-router-dom"; 
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Jobs Portal" },
+    { name: "description", content: "Find your dream job easily." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen ">
+
+      <Link
+        to="/job/1" 
+        className="mt-6 px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+      >
+        View Job Details
+      </Link>
+    </div>
+  );
 }
