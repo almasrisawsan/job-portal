7c8d88f2bae411826c3a237256582893892c8780
m7md-Y-3mra <kingmoha2002@gmail.com>
2025-10-23 15:43:44 +0300

    feat: Refactor folder structure

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,10 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  layout("./layout.tsx", [
+    index("routes/Home/home.tsx"),
+    route("/job", "routes/JobDetails/JobDetails.jsx"),
+    route("/create-job", "routes/CreateJob/CreateJob.jsx"),
+    route("/dashboard", "routes/Dashboard/Dashboard.jsx"),
+  ]),
+] satisfies RouteConfig;
--- app/routes/Home/home.tsx
+++ app/routes/Home/home.tsx
@@ -1,4 +1,5 @@
 import type { Route } from "./+types/home";
+import HeroSection from "./components/HeroSection";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,5 +9,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <h1>Job Portal</h1>;
+  return (
+    <div>
+      <HeroSection />
+    </div>
+  );
 }
