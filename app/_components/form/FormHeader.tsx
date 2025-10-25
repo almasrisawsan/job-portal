bc991981beb084fc2f1148cd5d51542c5ab78996
Moamen-Yazouri <moaamenalyazouro@gmail.com>
2025-10-25 15:51:50 +0200

    refactor(form): add type annotation for title prop in FormHeader

--- app/_components/form/FormHeader.tsx
+++ app/_components/form/FormHeader.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function FormHeader({ title }) {
+function FormHeader({ title }: { title: string }) {
   return (
     <div className="bg-gray-100 flex justify-center items-center py-6">
       <h3 className="text-xl font-semibold text-gray-700">{title}</h3>
