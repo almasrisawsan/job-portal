847c477c84d3b64493abd2306baba2191928485a
khaledabosaeed <kahledblata242@gmail.com>
2025-10-25 12:00:17 +0300

    Cannot find module 'react-router-dom' imported from '/home/khaled/code/GSG-training/shared-group/job-portal/app/components/FeaturedJobs.jsx'  what the error
     solve the error

--- app/Pages/DetailsJob.jsx
+++ app/Pages/DetailsJob.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
 import Button from "../components/Button";
 import { GetJobsByid } from "../api/api";
 
