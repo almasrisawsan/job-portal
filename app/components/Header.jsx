827e4f239cd1c1b1ee553a49416cdf1ea1f5d903
khaledabosaeed <kahledblata242@gmail.com>
2025-10-26 11:23:06 +0200

    header links

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "lucide-react";
 import Button from "./Button";
 
 export default function Header() {
@@ -7,9 +8,9 @@ export default function Header() {
         <h1 className="text-2xl font-bold text-primary">JobsPortal</h1>
         <div className="flex gap-2 items-center">
           <h2 className="text-primary text-sm font-medium">Dashboard</h2>
-          <Button className="rounded-md" size="md">
+          <a href={'/jobs/new'} className="rounded-md bg-primary text-white p-3" size="md">
             Post a job
-          </Button>
+          </a>
         </div>
       </div>
     </header>
