89f0d7999aa6798cb264727d7ffc8e9cc6e43b02
alaaAdel <alaaward3000@gmail.com>
2025-10-26 13:29:38 +0200

    add ink to homeage  in header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,10 +1,13 @@
 import Button from "./Button";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="bg-white shadow-sm py-4 w-full fixed top-0 z-50">
       <div className="container mx-auto flex items-center justify-between">
+        <Link to="/">
         <h1 className="text-2xl font-bold text-primary">JobsPortal</h1>
+        </Link>
         <div className="flex gap-2 items-center">
           <h2 className="text-primary text-sm font-medium">Dashboard</h2>
           <Button className="rounded-md" size="md">
