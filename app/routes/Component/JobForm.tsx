c067f80203b9f3ab1919d4884c8c025d6899cf23
AbdullahLaham <abed26194@gmail.com>
2025-10-28 10:45:24 +0200

    feat: creating the jobForm page and sending the data with the post API

--- app/routes/Component/JobForm.tsx
+++ app/routes/Component/JobForm.tsx
@@ -2,9 +2,54 @@
 import React from "react";
 import Button from "./Button";
 import Containar from "./Containar";
+import Footer from "./Footer";
+interface JobFormProps {
+  companyName: string;
+  companyWebsite: string;
+  jobTitle: string;
+  jobCategory: "Technology" | "Design" | "Marketing";
+  jobType: "Full Time" | "Part Time" | "Hourly";
+  jobLocation: string;
+  experience: string;
+  salaryRange: string;
+  featured: "Yes" | "No";
+  jobDescription: string;
+  setCompanyName: React.Dispatch<React.SetStateAction<string>>;
+  setCompanyWebsite: React.Dispatch<React.SetStateAction<string>>;
+  setJobTitle: React.Dispatch<React.SetStateAction<string>>;
+  setJobCategory: React.Dispatch<React.SetStateAction<string>>;
+  setJobType: React.Dispatch<React.SetStateAction<string>>;
+  setJobLocation: React.Dispatch<React.SetStateAction<string>>;
+  setExperience: React.Dispatch<React.SetStateAction<string>>;
+  setSalaryRange: React.Dispatch<React.SetStateAction<string>>;
+  setFeatured: React.Dispatch<React.SetStateAction<string>>;
+  setJobDescription: React.Dispatch<React.SetStateAction<string>>;
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
 
-import Footer from  "./Footer"
-const JobForm = () => {
+}
+const JobForm: React.FC<JobFormProps> = ({
+  companyName,
+  setCompanyName,
+  companyWebsite,
+  setCompanyWebsite,
+  jobTitle,
+  setJobTitle,
+  jobCategory,
+  setJobCategory,
+  jobType,
+  setJobType,
+  jobLocation,
+  setJobLocation,
+  experience,
+  setExperience,
+  salaryRange,
+  setSalaryRange,
+  featured,
+  setFeatured,
+  jobDescription,
+  setJobDescription,
+  handleSubmit,
+}) => {
   return (
     <>
       <div className="min-h-screen bg-gray-50 pt-8">
@@ -18,7 +63,7 @@ const JobForm = () => {
 
           {/* Form */}
           <Containar>
-            <form className="p-6 space-y-6 shadow-md m-2">
+            <form onSubmit={handleSubmit} className="p-6 space-y-6 shadow-md m-2">
               {/* Company Name & Website */}
               <div className="flex flex-col md:flex-row gap-4">
                 <div className="flex-1">
@@ -27,6 +72,8 @@ const JobForm = () => {
                   </h2>
                   <input
                     type="text"
+                    value={companyName}
+                    onChange={(e) => setCompanyName(e.target.value)}
                     placeholder="Name"
                     className="w-full px-3 py-2 text-sm border text-gray-800  border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-blue-500"
                   />
@@ -38,6 +85,8 @@ const JobForm = () => {
                   </h2>
                   <input
                     type="text"
+                    value={companyWebsite}
+                    onChange={(e) => setCompanyWebsite(e.target.value)}
                     className="w-full px-3 py-2 text-sm border text-gray-800  border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-blue-500"
                     placeholder="Website Link"
                   />
@@ -51,6 +100,8 @@ const JobForm = () => {
                 </h2>
                 <input
                   type="text"
+                  value={jobTitle}
+                  onChange={(e) => setJobTitle(e.target.value)}
                   placeholder="Title"
                   className="w-full px-3 py-2 text-sm border text-gray-800  border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-blue-500"
                 />
@@ -62,7 +113,10 @@ const JobForm = () => {
                   <h2 className="text-sm font-semibold text-gray-800 mb-3">
                     Job Category
                   </h2>
-                  <select className="w-full px-3 py-2 text-sm border text-gray-800  border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-blue-500">
+                  <select
+                    value={jobCategory}
+                    onChange={(e) => setJobCategory(e.target.value)}
+                    className="w-full px-3 py-2 text-sm border text-gray-800  border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-blue-500">
                     <option>Technology</option>
                     <option>Design</option>
                     <option>Marketing</option>
@@ -73,7 +127,8 @@ const JobForm = () => {
                   <h2 className="text-sm font-semibold text-gray-800 mb-3">
                     Job Type
                   </h2>
-                  <select className="w-full px-3 text-sm py-2 border text-black border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-blue-500">
+                  <select value={jobType}
+              onChange={(e) => setJobType(e.target.value)} className="w-full px-3 text-sm py-2 border text-black border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-blue-500">
                     <option>Full Time</option>
                     <option>Part Time</option>
                     <option>Hourly</option>
@@ -89,6 +144,8 @@ const JobForm = () => {
                   </h2>
                   <input
                     type="text"
+                    value={jobLocation}
+              onChange={(e) => setJobLocation(e.target.value)}
                     placeholder="Location"
                     className="w-full px-3 py-2 text-gray-800  text-sm border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-blue-500"
                   />
@@ -100,6 +157,8 @@ const JobForm = () => {
                   </h2>
                   <input
                     type="text"
+                    value={experience}
+              onChange={(e) => setExperience(e.target.value)}
                     placeholder="Experience"
                     className="w-full px-3 py-2 text-gray-800  text-sm border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-blue-500"
                   />
@@ -114,6 +173,8 @@ const JobForm = () => {
                   </h2>
                   <input
                     type="text"
+                    value={salaryRange}
+              onChange={(e) => setSalaryRange(e.target.value)}
                     placeholder="Salary Range"
                     className="w-full px-3 py-2 text-sm text-gray-800  border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-blue-500"
                   />
@@ -123,7 +184,10 @@ const JobForm = () => {
                   <h2 className="text-sm font-semibold text-gray-800 mb-3">
                     Featured
                   </h2>
-                  <select className="w-full px-3 text-sm py-2 border text-gray-800  border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-blue-500">
+                  <select
+                  value={featured}
+              onChange={(e) => setFeatured(e.target.value)}
+                   className="w-full px-3 text-sm py-2 border text-gray-800  border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-blue-500">
                     <option>Yes</option>
                     <option>No</option>
                   </select>
@@ -137,6 +201,8 @@ const JobForm = () => {
                 </h2>
                 <textarea
                   rows={4}
+                  value={jobDescription}
+              onChange={(e) => setJobDescription(e.target.value)}
                   placeholder="Job Description"
                   className="w-full h-[300px] px-3  text-gray-800 py-2 text-sm border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-blue-500"
                 ></textarea>
@@ -144,7 +210,7 @@ const JobForm = () => {
 
               {/* Submit Button */}
               <div className="flex justify-end pt-4 mt-[20px]">
-                <Button />
+                <button type="submit" className="cursor-pointer px-4 py-2 bg-[#338573] text-white rounded hover:bg-[#246154] active:scale-95 transition">Post Job</button>
               </div>
             </form>
           </Containar>
--- app/routes/jobs/create-job.tsx
+++ app/routes/jobs/create-job.tsx
@@ -1,3 +1,75 @@
+import { useState } from "react";
+import JobForm from "../Component/JobForm";
+import axios from "axios";
+
 export default function CreateJobPage() {
-  return <div>CreateJobPage</div>;
+  const [companyName, setCompanyName] = useState("");
+  const [companyWebsite, setCompanyWebsite] = useState("");
+  const [jobTitle, setJobTitle] = useState("");
+  const [jobCategory, setJobCategory] = useState("Technology");
+  const [jobType, setJobType] = useState("Full Time");
+  const [jobLocation, setJobLocation] = useState("");
+  const [experience, setExperience] = useState("");
+  const [salaryRange, setSalaryRange] = useState("");
+  const [featured, setFeatured] = useState("Yes");
+  const [jobDescription, setJobDescription] = useState("");
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    const jobData = {
+      companyName,
+      companyWebsite,
+      jobTitle,
+      jobCategory,
+      jobType,
+      jobLocation,
+      experience,
+      salaryRange,
+      featured,
+      jobDescription,
+    };
+
+    console.log("Posting job data:", jobData);
+
+    // ✅ Real API POST
+    // const response = await fetch("https://68f8f8e8deff18f212b83fba.mockapi.io/jobs", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/json" },
+    //   body: JSON.stringify(jobData),
+    // });
+    const res = await axios.post("https://68f8f8e8deff18f212b83fba.mockapi.io/jobs",jobData);
+    console.log("API Response:", res.data);
+
+
+    const result = res.data;
+    console.log("✅ Fake API Response:", result);
+
+    alert("✅ Job created successfully!");
+  };
+
+  return (
+    <JobForm
+      companyName={companyName}
+      setCompanyName={setCompanyName}
+      companyWebsite={companyWebsite}
+      setCompanyWebsite={setCompanyWebsite}
+      jobTitle={jobTitle}
+      setJobTitle={setJobTitle}
+      jobCategory={jobCategory}
+      setJobCategory={setJobCategory}
+      jobType={jobType}
+      setJobType={setJobType}
+      jobLocation={jobLocation}
+      setJobLocation={setJobLocation}
+      experience={experience}
+      setExperience={setExperience}
+      salaryRange={salaryRange}
+      setSalaryRange={setSalaryRange}
+      featured={featured}
+      setFeatured={setFeatured}
+      jobDescription={jobDescription}
+      setJobDescription={setJobDescription}
+      handleSubmit={handleSubmit}
+    />
+  );
 }
