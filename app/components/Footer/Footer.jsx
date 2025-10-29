d29b313bec6f3c1be407e7b471ea34a57f76d8bb
Mohammed Yousef <69032001+m7md-Y-3mra@users.noreply.github.com>
2025-10-29 21:07:36 +0200

    Revert "Hussein mohammed"

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -2,10 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div
-      className="flex justify-center items-center p-4 
-   text-amber-50 bg-[#338573] shadow"
-    >
+    <div className="flex justify-center items-center p-4 bg-[#338573] shadow text-white">
       <p>© 2025 Jobs Portal. Designed By Wilson Kinyua.</p>
     </div>
   );
